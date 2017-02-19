9ab19c5ef336e7a3e39dd588e49fe1126d314223
Matz Wiik <matzwiik@gmail.com>
2017-02-19 23:27:19 +0100

    Added routing with React Router

--- client/main.js
+++ client/main.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Bins} from '../imports/collections/bins';
-
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import BinsMain from './components/bins/bins_main';
+import BinsList from './components/bins/bins_list';
 import App from './components/app'
 
+const routes = (
+    <Router history={browserHistory}>
+        <Route path="/" component={App}>
+            <Route path="bins" components={BinsMain}>
+            </Route>
+        </Route>
+    </Router>
+);
+
 Meteor.startup(() =>{
-    ReactDOM.render(<App />, document.querySelector('.render-target'));
+    ReactDOM.render(routes, document.querySelector('.render-target'));
 });
